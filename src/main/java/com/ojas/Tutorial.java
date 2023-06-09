e3d32f3ea9b473b572c71cabab7a4e6ec8c88341
ra21005 <ramesh.adepu@ojas-it.com>
2023-06-09 14:01:54 +0530

    file added

---
+++ src/main/java/com/ojas/Tutorial.java
@@ -0,0 +1,11 @@
+package com.ojas;
+
+import java.util.logging.Logger;
+
+public class Tutorial {
+
+	public static void main(String[] args) {
+		Logger.getLogger("Application Started");
+	}
+
+}
---
+++ src/test/java/com/ojas/test/TutorialTest.java
@@ -0,0 +1,5 @@
+package com.ojas.test;
+
+public class TutorialTest {
+
+}
